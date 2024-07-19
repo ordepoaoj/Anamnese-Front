7e93309d321f4660cf25ee42092e279a9212d2a0
GabrielJVB <gabrieljr0310@gmail.com>
2024-07-18 21:32:43 -0300

    Inicio Chatbot
    Inicio chatbot "botonic"

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,18 @@ import App from './Home'; // Importe o componente principal da aplicação
 import AnamneseLogin from './pages/login/login';
 import Formulario from './pages/form/form';
 import Register from './pages/login/register';
+import { Text, Reply } from '@botonic/react'
 
 const AppRoutes = () => {
     console.log('AppRoutes');
   return (
     <BrowserRouter>
+      <Text>Hello! Nice to meet you ;</Text>
+        <Text>
+          How can I help you?
+          <Reply payload='search'>Search product</Reply>
+          <Reply payload='track'>Track my order</Reply>
+        </Text>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path='/register' element={<Register />} />
